dae11ad8dd9c0f403f973f422343fadf60c78a0c
byt101 <bunnnyg09@gmail.com>
2024-11-12 01:28:16 +0530

    Update Ass3_Netbeans.java

--- Ass3_Netbeans.java
+++ Ass3_Netbeans.java
@@ -2,6 +2,8 @@ Procedure :-
 netbeans ide :
 file-> new project-> java with maven & java application-> project_name-> source package-> new-> jframe form-> frame_name-> finish-> setup text fields with labels->button(add)-> double click->paste code-> save->run;
 
+code :
+
 int a,b,c;
 a = Integer.parseInt(jTextField1.getText());
 b = Integer.parseInt(jTextField2.getText());
