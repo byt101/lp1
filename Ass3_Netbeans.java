cf6c491398515436c531b8168c30ad9d8bbc682f
byt101 <bunnnyg09@gmail.com>
2024-11-12 01:26:20 +0530

    Add files via upload

---
+++ Ass3_Netbeans.java
@@ -0,0 +1,9 @@
+Procedure :-
+netbeans ide :
+file-> new project-> java with maven & java application-> project_name-> source package-> new-> jframe form-> frame_name-> finish-> setup text fields with labels->button(add)-> double click->paste code-> save->run;
+
+int a,b,c;
+a = Integer.parseInt(jTextField1.getText());
+b = Integer.parseInt(jTextField2.getText());
+c = a+b;
+jTextField3.setText(" "+c);
